efd2a481928859bb43bac39a7a378b304af77299
lovfiny <pv62020@yandex.ru>
2026-05-26 12:16:04 +0300

    Create mockData.js
    Добавлены моки товаров на главной страницы

---
+++ js/mockData.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    id: 1,
+    name: "Бузинная палочка",
+    brand: "Олливандер",
+    price: 5000,
+    required_level: 3,
+    delivery_type: "OWL",
+    category_id: 8,
+    image_url: "/images/wand.png",
+    shop_id: 1,
+    emoji: "🪄"
+  },
+  {
+    id: 2,
+    name: "Амортенция",
+    brand: "Слагхорн",
+    price: 700,
+    required_level: 5,
+    delivery_type: "OWL",
+    category_id: 1,
+    image_url: "/images/potion.png",
+    shop_id: 2,
+    emoji: "🔮"
+  },
+  {
+    id: 3,
+    name: "Учебник заклинаний",
+    brand: "Флитвик",
+    price: 300,
+    required_level: 1,
+    delivery_type: "PIGEON",
+    category_id: 3,
+    image_url: "/images/book.png",
+    shop_id: 3,
+    emoji: "📚"
+  },
+  {
+    id: 4,
+    name: "Нимбус 2000",
+    brand: "Нимбус",
+    price: 400,
+    required_level: 2,
+    delivery_type: "OWL",
+    category_id: 5,
+    image_url: "/images/broom.png",
+    shop_id: 4,
+    emoji: "🧹"
+  },
+  {
+    id: 5,
+    name: "Сова-почтальон",
+    brand: "Совы Хогвартса",
+    price: 200,
+    required_level: 1,
+    delivery_type: "OWL",
+    category_id: 7,
+    image_url: "/images/owl.png",
+    shop_id: 5,
+    emoji: "🦉"
+  },
+  {
+    id: 6,
+    name: "Мантия-невидимка",
+    brand: "Дара Смерти",
+    price: 10000,
+    required_level: 7,
+    delivery_type: "PHOENIX",
+    category_id: 6,
+    image_url: "/images/cloak.png",
+    shop_id: 6,
+    emoji: "👻"
+  }
+];
