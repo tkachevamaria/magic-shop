00f7ce98c2714ddc41df332a8b7f095758975387
tkachevamaria <tkatschevamascha@gmail.com>
2026-06-01 15:35:02 +0300

    коммент от препода

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -89,6 +89,7 @@ func main() {
 	r.Get("/api/products/{id}", productHandler.GetProductByID)
 
 	// Защищенные маршруты
+	//сколько живет токен
 	r.Group(func(r chi.Router) {
 		r.Use(auth.Middleware)
 
